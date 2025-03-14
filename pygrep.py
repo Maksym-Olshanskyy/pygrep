ee434542e9941ce75a9b1e684bbe02241b361b9c
Maksym-Olshanskyy <mxolshanskyy@students.everettcc.edu>
2025-03-14 00:04:25 -0700

    added tail

--- pygrep.py
+++ pygrep.py
@@ -4,34 +4,49 @@ import time
 # args
 findString = ""
 lineNumbers = False
+tail = False
 tailLen = 0
 
 #argument handling code
 abort = False
+getHelp = False
 i = 1
 while i < len(sys.argv):
     match sys.argv[i]:
         case "-l":
             lineNumbers = True
         case "-t":
+            tail = True
             i += 1
             if i < len(sys.argv):
                 tailLen = int(sys.argv[i])
             else:
                 print("pygrep: no quantity of lines given for -t, Aborting.")
                 abort = True
+                getHelp = True
+        case "-h":
+            print("Usage: [input text] | python pygrep [-h][-t num][-l] \"search string\"")
+            print("  -h     : you are here!")
+            print("  -l     : prints line numbers in front of each line")
+            print("  -t num : tail, quantity of lines to print after a line with a match. ")
         case _ :
             if sys.argv[i][0] == "-":
                 print("pygrep: option " + sys.argv[i] + " is not recognized, Ignored.")
+                getHelp = True
             elif findString == "":
                 findString = sys.argv[i]
             else:
-                print("pygrep: invalid input: " + sys.argv[i] + ", Ignored.")      
+                print("pygrep: invalid input: " + sys.argv[i] + ", Ignored.")
+                getHelp = True
     i += 1
 
 if findString == "":
     print("pygrep: no search string given, Aborting.")
     abort = True
+    getHelp = True
+
+if getHelp:
+    print("pygrep -h for help")
 
 if not abort:
     
@@ -52,6 +67,7 @@ if not abort:
 
     #printArr()
     stringLen = len(findString)
+    nonMatchLines = tailLen
     #print("Output is: ")
     # searching code
     for i in range(len(inputText)): # for each line in inputText
@@ -76,9 +92,11 @@ if not abort:
                 foundString = True
             j += 1
 
-        if printLine:
+        if printLine: # if there is an occurance, reset tail length
+            nonMatchLines = 0
             print(inputText[i])
             printLine = False
-
-else:
-    print("pygrep -h for help")
+        elif nonMatchLines < tailLen: # if tail length hasn't reached tailLen, keep printing lines
+            nonMatchLines += 1
+            print(inputText[i])
+            printLine = False
