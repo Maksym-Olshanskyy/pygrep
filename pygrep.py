a4a7b1286008c5eb629d63d5e5ac833a4e31e0f4
Maksym-Olshanskyy <mxolshanskyy@students.everettcc.edu>
2025-03-13 19:52:30 -0700

    Has many issues. this algo is not the way to go

--- pygrep.py
+++ pygrep.py
@@ -1,1 +1,43 @@
-print("Hello!")
+import sys
+
+inputText = []
+findString = "hello"
+
+def printArr():
+    print('Input is:')
+    for i in inputText: 
+        print(i)
+
+#read & store each line as individual item in list
+while True:
+        try:
+            inputText.append(input())
+        except EOFError:
+            # no more information
+            break
+
+
+printArr()
+
+for i in range(len(inputText)): # for each line in inputText
+    currLine = inputText[i]
+    stringLen = len(findString)
+    stringIndex = 0
+    foundString = False
+
+    for j in range(len(currLine)): # for each char in line
+        if currLine[j] == findString[stringIndex]:
+            stringIndex += 1
+        else:
+            stringIndex = 0
+        if stringIndex >= stringLen:
+            foundString = True
+            print("Index " + str(j - stringLen + 1) + " - " + str(j) + ", ", end="")
+            stringIndex = 0
+    
+    if foundString:
+        print(inputText[i])
+        foundString = False
+
+print()
+     
