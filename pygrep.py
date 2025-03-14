1f0551a06e649e1c575e2d757d9b427078c7df61
Maksym-Olshanskyy <mxolshanskyy@students.everettcc.edu>
2025-03-13 22:29:41 -0700

    working arguments

--- pygrep.py
+++ pygrep.py
@@ -1,52 +1,84 @@
 import sys
+import time
 
-inputText = []
-findString = "hello"
-print(str(len(findString)))
+# args
+findString = ""
+lineNumbers = False
+tailLen = 0
 
-def printArr():
-    print('Input is:')
-    for i in inputText: 
-        print(i)
+#argument handling code
+abort = False
+i = 1
+while i < len(sys.argv):
+    match sys.argv[i]:
+        case "-l":
+            lineNumbers = True
+        case "-t":
+            i += 1
+            if i < len(sys.argv):
+                tailLen = int(sys.argv[i])
+            else:
+                print("pygrep: no quantity of lines given for -t, Aborting.")
+                abort = True
+        case _ :
+            if sys.argv[i][0] == "-":
+                print("pygrep: option " + sys.argv[i] + " is not recognized, Ignored.")
+            elif findString == "":
+                findString = sys.argv[i]
+            else:
+                print("pygrep: invalid input: " + sys.argv[i] + ", Ignored.")      
+    i += 1
 
-#read & store each line as individual item in list
-while True:
-        try:
-            inputText.append(input())
-        except EOFError:
-            # no more information
-            break
+if findString == "":
+    print("pygrep: no search string given, Aborting.")
+    abort = True
 
-
-printArr()
-stringLen = len(findString)
-print("Output is: ")
-for i in range(len(inputText)): # for each line in inputText
-    currLine = inputText[i]
+if not abort:
     
-    foundString = True
-    printLine = False
+    inputText = []
+    def printArr():
+        print('Input is:')
+        for i in inputText: 
+            print(i)
 
-    #for j in range(): # for each char in line
-    j = 0
-    while j <= len(currLine) - stringLen:
-        print("Interation: " + str(j))
-        for stringIndex in range(stringLen):
-            if currLine[j + stringIndex] != findString[stringIndex]:
-                foundString = False
-                break # don't keep searching if string isn't matching
+    #read & store each line as individual item in list
+    while True:
+            try:
+                inputText.append(input())
+            except EOFError:
+                # no more information
+                break
+
+
+    #printArr()
+    stringLen = len(findString)
+    #print("Output is: ")
+    # searching code
+    for i in range(len(inputText)): # for each line in inputText
+        currLine = inputText[i]
         
-        if foundString:
-            printLine = True
-            print("Index " + str(j) + " - " + str(j + stringLen -1) + ", ", end="")
-            j += stringLen -1
-        else:
-            foundString = True
-        j += 1
-
-    if printLine:
-        print(inputText[i])
+        foundString = True
         printLine = False
 
-print()
-     
+        #for j in range(): # for each char in line
+        j = 0
+        while j <= len(currLine) - stringLen:
+            #print("Interation: " + str(j))
+            for stringIndex in range(stringLen):
+                if currLine[j + stringIndex] != findString[stringIndex]:
+                    foundString = False
+                    break # don't keep searching if string isn't matching
+            if foundString:
+                printLine = True
+                print("Index " + str(j) + " - " + str(j + stringLen -1) + ", ", end="")
+                j += stringLen -1 # jump over found string, because finding the string inside itself is impossible.
+            else:
+                foundString = True
+            j += 1
+
+        if printLine:
+            print(inputText[i])
+            printLine = False
+
+else:
+    print("pygrep -h for help")
